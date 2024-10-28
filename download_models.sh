79cf0e8b11662ee20f8ab894d2899b530138fc84
Thomas Fritz <mail@nonelabs.com>
2024-10-28 16:44:40 +0100

    Initial commit

---
+++ download_models.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://huggingface.co/bartowski/gemma-2-27b-it-GGUF/resolve/main/gemma-2-27b-it-Q5_K_M.gguf?download=true -O models/gemma-2-27b-it-Q5_K_M.gguf
+wget https://huggingface.co/bartowski/gemma-2-27b-it-GGUF/resolve/main/gemma-2-27b-it-IQ2_M.gguf?download=true -O models/gemma-2-27b-it-IQ2_M.gguf
+wget https://huggingface.co/bartowski/gemma-2-27b-it-GGUF/resolve/main/gemma-2-27b-it-Q3_K_L.gguf?download=true -O models/gemma-2-27b-it-Q3_K_L.gguf
